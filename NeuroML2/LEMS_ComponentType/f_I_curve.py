94bb5f88eca8d4062c9e146ecce06991a6690c31
Andris Ecker <ecker.andris@gmail.com>
2016-03-30 21:57:13 +0100

    f_i curve added

---
+++ NeuroML2/LEMS_ComponentType/f_I_curve.py
@@ -0,0 +1,18 @@
+import sys
+from pyneuroml.analysis import generate_current_vs_frequency_curve
+    
+nogui = '-nogui' in sys.argv  # Used to supress GUI in tests for Travis-CI
+    
+generate_current_vs_frequency_curve(nml2_file =           'WangBuzsakiCell.xml', 
+                                    cell_id =             'wb1', 
+                                    start_amp_nA =         0.0, 
+                                    end_amp_nA =           0.02, 
+                                    step_nA =              0.005, 
+                                    analysis_duration =    100, 
+                                    analysis_delay =       50,
+                                    temperature =          "35degC",
+                                    spike_threshold_mV =   0.0,
+                                    plot_voltage_traces =  not nogui,
+                                    plot_if =              True,  # not nogui,
+                                    plot_iv =              True) #,  # not nogui,
+                                    # simulator="jNeuroML")  
