82577d7fa79dcf246ba0bad2d5c9e5cfa3d62fa2
Andris Ecker <ecker.andris@gmail.com>
2016-04-01 16:47:33 +0100

    creates valid xml

--- NeuroML2/LEMS_ComponentType/WangBuzsakiNet.py
+++ NeuroML2/LEMS_ComponentType/WangBuzsakiNet.py
@@ -6,90 +6,105 @@ from neuroml import __version__
 from pyneuroml import pynml
 from pyneuroml.lems.LEMSSimulation import LEMSSimulation
 
-import random
-random.seed(43297)
+import numpy.random as rnd
+rnd.seed(43297)
 
 ref = 'WangBuzsaki'
 nml_doc = neuroml.NeuroMLDocument(id=ref)
 
-# Defie Wang - Buzsáki 1996 PV+ basket cell
-def wangBuzsaiBasketCell(id, r, C, v0, gL, eL, spikeThresh,
-                        gNa, eNa, alpham_rate, alpham_midpoint, alpham_scale, betam_rate, betam_midpoint_betam_scale,
-                        h_q10, alphah_rate, alphah_midpoint, alphah_scale, betah_rate, betah_scale, betah_midpoit,
-                        gK, eK, n_q10, alphan_rate, alphan_midpoint, alphan_scale1, alphan_scale2,
-                        betan_rate, betan_midpoint, betan_scale):
-                      
-                            
-# Define Wang - Buzsáki 1996 synapse
-def wangBuzsakiSynapse(id, g_s, e_s, Vth, Vscale, alpha, beta):
-    
-
-
 # Define Wang - Buzsáki 1996 network
-def generate_WB_network(network_id           = ref+'netwok',
-                    cell_id,
+def generate_WB_network(cell_id,
                     synapse_id,
-                    numCells_bc              = 100,
-                    connection               = True,
-                    connection_probablity,
+                    connection_probability,
                     I_mean,
                     I_sigma,
-                    temperature              = 37 degC,
+                    generate_LEMS_simulation,
+                    x_size                   = 100,
+                    y_size                   = 100,
+                    z_size                   = 100,
+                    network_id               = ref+'Network',
+                    numCells_bc              = 10,
+                    color                    = '0 0 1',
+                    connection               = True,
+                    temperature              = '37 degC',
                     validate                 = True,
-                    generate_LEMS_simulation = True,
-                    duration                 = 100,  # ms
+                    duration                 = 500,  # ms
                     dt                       = 0.01):
     
     nml_doc = neuroml.NeuroMLDocument(id=network_id)
+    nml_doc.includes.append(neuroml.IncludeType(href='WangBuzsakiCell.xml'))
+    nml_doc.includes.append(neuroml.IncludeType(href='WangBuzsakiSynapse.xml'))
+    
     
     # Create network
     net = neuroml.Network(id=network_id, type='networkWithTemperature', temperature=temperature)
     net.notes = 'Network generated using libNeuroML v%s'%__version__
     nml_doc.networks.append(net)
     
+    
     # Create population
-    pop = neoroml.Population(id=ref+'pop', component=cell_id, type='populationList', size=numCells_bc, color='None')
-    if color in not None:
-        pop.properties.append(Property('color', color))
+    pop = neuroml.Population(id=ref+'pop', component=cell_id, type='populationList', size=numCells_bc)
+    if color is not None:
+        pop.properties.append(neuroml.Property('color', color))
     net.populations.append(pop)
     
+    for i in range(0, numCells_bc):
+        inst = neuroml.Instance(id=i)
+        pop.instances.append(inst)
+        inst.location = neuroml.Location(x=str(x_size*rnd.random()), y=str(y_size*rnd.random()), z=str(z_size*rnd.random()))
+        
+    
     # Add connections
-    proj = neuroml.Projection(id=ref+'proj' synapse=synapse_id, presynaptic_population=pop, postsynaptic_population=pop)
+    proj = neuroml.Projection(id=ref+'proj', synapse=synapse_id, presynaptic_population=pop.id, postsynaptic_population=pop.id)
     
     conn_count = 0
-    for in in range(0, numCells_bs):
-        for j in range(0, numCell_bs):
-            if i != j and rand() < connection_probability:
-                connection = neuroml.Connection(id=conn_count, pre_cell_id='../%s[%i]'%(pop, i), pro_cell_id='../%s[%i]'%(pop, j))
+    for i in range(0, numCells_bc):
+        for j in range(0, numCells_bc):
+            if i != j and rnd.random() < connection_probability:
+                connection = neuroml.Connection(id=conn_count, pre_cell_id='../%s[%i]'%(pop.id, i), post_cell_id='../%s[%i]'%(pop.id, j))
                 proj.connections.append(connection)
                 conn_count += 1
                 
-    net.projection.append(proj)
+    net.projections.append(proj)
     
-    # Add outer input
-    for i in range(0, numCells_bs):
-        expInp = neuroml.ExplicitInput(target='%s[%i]'%(pop.id, i), input= , destination='synapses')
+    
+    # Add outer input (IClamp)
+    tmp = rnd.normal(I_mean, I_sigma**2, numCells_bc)  # random numbers from Gaussian distribution
+    for i in range(0, numCells_bc):
+        pg = neuroml.PulseGenerator(id='IClamp%i'%i, delay='0ms', duration='%sms'%duration, amplitude='%fpA'%(tmp[i]))
+        
+        nml_doc.pulse_generators.append(pg)
+    
+        input_list = neuroml.InputList(id='input%i'%i, component='IClamp_%i'%i, populations=pop.id)
+        input = neuroml.Input(id=i, target='%s[%i]'%(pop.id, i), destination='wbs1')
+        input_list.input.append(input)
+        
+        net.input_lists.append(input_list)
+    
     
     # Write to file
     nml_file = '%sNet.nml'%ref
     print 'Writing network file to:', nml_file, '...'
-    neuroml.writers.NeoroMLWriter.write(nml_doc, nml_file)
+    neuroml.writers.NeuroMLWriter.write(nml_doc, nml_file)
+    
     
     if validate:
         
         # Validate the NeuroML
-        neuroml.utils.validate_neuroml2(nml_file)
-        
+        from neuroml.utils import validate_neuroml2
+        validate_neuroml2(nml_file) 
+    
+    
     if generate_LEMS_simulation:
         
         # Vreate a LEMSSimulation to manage creation of LEMS file
-        ls = LEMSSimulation('LEMS_%s'%network_id, duration, dt)
+        ls = LEMSSimulation(network_id, duration, dt)
         
         # Point to network as target of simulation
         ls.assign_simulation_target(net.id)
         
         # Incude generated/existing NeuroML2 files
-        ls.include_neuroml2_file(nml_file)
+        ls.include_neuroml2_file(nml_file, include_included=False)
         
         # Specify Display and output files
         disp_bc = 'display_bc'
@@ -98,12 +113,15 @@ def generate_WB_network(network_id           = ref+'netwok',
         of_bc = 'volts_file_bc'
         ls.create_output_file(of_bc, 'wangbuzsaki_network.dat')
         
+        of_spikes_bc = 'of_spikes_bc'
+        ls.create_event_output_file(of_spikes_bc, 'wangbuzsaki_network_spikes.dat')
+        
         max_traces = 25
-        for i in range(size):
+        for i in range(numCells_bc):
             quantity = '%s[%i]/v'%(pop.id, i)
             if i < max_traces:
-                ls.add_line_to_display(disp_bc, 'BC %i: Vm'%i, quantitiy, '1mV', pynml.get_next_hex_color())
-            ls.add_column_to_outputfile(of_bc, 'v_%i'%i, quantity)
+                ls.add_line_to_display(disp_bc, 'BC %i: Vm'%i, quantity, '1mV', pynml.get_next_hex_color())
+            ls.add_column_to_output_file(of_bc, 'v_%i'%i, quantity)
             
         # Save to LEMS file
         print 'Writing LEMS file...'
@@ -112,18 +130,20 @@ def generate_WB_network(network_id           = ref+'netwok',
     else:
         
         ls = None
+        lems_file_name = ''
                 
     return ls, lems_file_name
 
 
 if __name__ == '__main__':
     
-    ls, lems_file_name = generate_WB_network(cell_id=, synapse_id=, connection_probability=0.6, I_mean=1, I_sigma=0.1)
+    generate_LEMS_simulation = True
     
-    # run with jNeuroML
-    sim = pynml.run_lems_with_jneuroml(lems_file_name, nogui='True', load_saved_data='False', plot='True')
+    ls, lems_file_name = generate_WB_network('wb1', 'wbs1', 0.6, 1, 0.1, generate_LEMS_simulation)
     
-    # run with jNeuroML_NEURON
-    # sim = pynml.run_lems_with_jneuroml_neuron(lems_file_name, nogui='True', load_saved_data='False', plot='True')
-            
+    # if generate_LEMS_simulation:
+        # run with jNeuroML
+        # sim = pynml.run_lems_with_jneuroml(lems_file_name, nogui='True', load_saved_data='False', plot='True')
     
+        # run with jNeuroML_NEURON
+        # sim = pynml.run_lems_with_jneuroml_neuron(lems_file_name, nogui='True', load_saved_data='False', plot='True')    
